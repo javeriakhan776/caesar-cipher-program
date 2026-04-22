b2917ba13834820b17eabb1a88e5b6cae3209419
javeria-khan <javeriaateeq007@gmail.com>
2026-04-22 15:22:48 +0500

    Commit 5: use fgets for full sentence input and preserve spaces by skipping shift on space characters

--- main.c
+++ main.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 #include<string.h>
 int main(){
-    char string[50]={'\0'};
+    char string[100]={'\0'};
     printf("Enter cipher text : ");
-    scanf("%s",&string);
+    fgets(string, 100, stdin);
     
-    for (int i=0; i<=50; i++){
+    for (int i=0; i<=99; i++){
         if (string[i]>64 && string[i]<91){
             string[i]+=32;
         }
@@ -14,9 +14,14 @@ int main(){
     for (int i=1; i<=25; i++){
         printf("\t%d . ",i);
         for (int j=0; j<=5; j++){
-            int temp=string[j]+i;
-            if(temp>122){temp-=26;}
-            printf("%c",temp);
+            if(string[j]==32){
+                printf("%c",string[j]);
+            }
+            else{
+                int temp=string[j]+i;
+                if(temp>122){temp-=26;}
+                printf("%c",temp);
+            }
         }
         printf("\n");
     }
@@ -26,12 +31,17 @@ int main(){
     printf("\nSelect most appropriate key from above list and enter it : ");
     scanf("%d",&key);
     
-    printf("\n\nThe plain text is:\t");
+    printf("\n\nThe plain text is:\v");
     
-    for (int i=0; i<=50 && string[i]!=0; i++){
-        int temp=string[i]+key;
-        if(temp>122){temp-=26;}
-        printf("%c",temp);
+    for (int i=0; i<=99 && string[i]!=0; i++){
+        if(string[i]==32){
+            printf("%c",string[i]);
+        }
+        else{
+            int temp=string[i]+key;
+            if(temp>122){temp-=26;}
+            printf("%c",temp);
+        }
     }
     
     
