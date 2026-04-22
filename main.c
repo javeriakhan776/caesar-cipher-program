ff5ad903f91a20a652f2d32e6eb9bc806ae6598d
javeria-khan <javeriaateeq007@gmail.com>
2026-04-22 15:03:03 +0500

    Commit 3: handle capital letters by converting to lowercase

--- main.c
+++ main.c
@@ -4,6 +4,13 @@ int main(){
     char string[50]={'\0'};
     printf("Enter code : ");
     scanf("%s",&string);
+    
+    for (int i=0; i<=50; i++){
+        if (string[i]>64 && string[i]<91){
+            string[i]+=32;
+        }
+    }
+    
     for (int i=1; i<=25; i++){
         printf("%d . ",i);
         for (int j=0; j<=5; j++){
